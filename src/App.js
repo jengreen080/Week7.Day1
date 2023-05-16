7364878de8810901f99c0cbc513f9d1435e0685c
Jennifer <jng-96@hotmail.co.uk>
2023-05-16 09:31:05 +0100

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    // 1️⃣
+import './App.css';           // 2️⃣
 
 function App() {
+  const name = "Jennifer"
+  const age = 26
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<div>
+  		<h1 className="title">Hello World!</h1>
+  		<p> This is a paragraph of text</p>
+      <p> My name is {name} and I'm {age} years old</p>
+  	</div>
+    
   );
-}
+}                           // 8️⃣
 
-export default App;
+export default App;           // 9️⃣
